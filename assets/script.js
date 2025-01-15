48fd07d383d11910859244a8d8eaca3e586009e4
Souhayl Taiss <frontend.souhayl@gmail.com>
2025-01-15 16:14:10 +0100

    formatting code and adding some comment

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,38 @@
-const box = document.querySelector('.box');
-const boxContainer = document.querySelector('.box-container');
+const boxContainer = document.querySelector(".box-container");
+const box = document.querySelector(".box");
 const inputFile = document.querySelector('input[type="file"]');
 const boxWidth = 20;
 const boxCounter = 26;
 
 boxContainer.style = `--num-div:${boxCounter};--box-width:${boxWidth}px;--transformPos:80px;`;
+// Create boxes
 function creatBoxes(imageUrl) {
   for (let i = 0; i < boxCounter; i++) {
-    const newBox = document.createElement('div');
-    const newDiv = document.createElement('div');
-    newDiv.style = `background-image: url(${imageUrl || "./assets/img1.jpg"}); background-position-x:${-i * boxWidth}px`;
-    newBox.classList.add('box');
+    const newBox = document.createElement("div");
+    const newDiv = document.createElement("div");
+
+    newDiv.style = `background-image: url(${
+      imageUrl || "./assets/img1.jpg"
+    });background-position-x:${-i * boxWidth}px`;
+    newBox.classList.add("box");
     newBox.appendChild(newDiv);
     boxContainer.appendChild(newBox);
     newBox.style = "--number: " + i;
   }
 }
 creatBoxes();
-inputFile.addEventListener('change', function () {
+
+// Change image
+inputFile.addEventListener("change", function () {
   const file = inputFile.files[0];
+
   if (file) {
-      const reader = new FileReader();
-      reader.onload = function (e) {
-          const imageUrl = e.target.result;
-          boxContainer.innerHTML = ""; // Clear existing boxes
-          creatBoxes(imageUrl);
-      };
-      reader.readAsDataURL(file);
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const imageUrl = e.target.result;
+      boxContainer.innerHTML = ""; // Clear existing boxes
+      creatBoxes(imageUrl);
+    };
+    reader.readAsDataURL(file);
   }
 });
-
-
-
